106351aa02763cf391528eb66037d21bf8ae4fc9
Dragomir Liviu Marian <32988491+MarianLiviu@users.noreply.github.com>
2017-11-07 15:41:06 +0200

    Update E3func.m

--- E3func.m
+++ E3func.m
@@ -2,11 +2,13 @@ function [ma,vs,M]=E3func(x,p)
 ma=0;
 s=0;
 k=0;
+%parcurgem vectorul si adunam intr-o variabila toate partile reale ale numerelor complexe
+%in acelasi timp tinem si un contor a cate numere au fost adunate
 for z=1:1:p
     s=s+real(x(z));
     k=k+1;
 end
-
+%se realizeaza media aritmetica
 ma=s/k;
 
 for c=1:1:p
